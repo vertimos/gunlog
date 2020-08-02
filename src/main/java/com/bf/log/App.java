86828abd33329703b76c528c0fea74c9b8ee12b0
bazines <bazines6@gmail.com>
2020-08-02 10:41:38 +0200

    POM cleanup + Java 11 configured

--- src/main/java/com/bf/log/App.java
+++ src/main/java/com/bf/log/App.java
@@ -1,13 +1,14 @@
 package com.bf.log;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
